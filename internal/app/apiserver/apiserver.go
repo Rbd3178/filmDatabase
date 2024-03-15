8af2070e618b8dfaab3d5186ba37339176dbaca6
Ilya V <ilya.vorontsov.2004@gmail.com>
2024-03-16 01:57:13 +0300

    add router config and first handler

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,11 +1,19 @@
 package apiserver
 
-import "github.com/sirupsen/logrus"
+import (
+	"io"
+	"net/http"
+
+	"github.com/Rbd3178/filmDatabase/internal/app/store"
+	"github.com/sirupsen/logrus"
+)
 
 // APIServer
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *http.ServeMux
+	store  *store.Store
 }
 
 // New
@@ -13,6 +21,7 @@ func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: http.NewServeMux(),
 	}
 }
 
@@ -21,12 +30,18 @@ func (s *APIServer) Start() error {
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
+
+	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
 	s.logger.Info("starting api server")
 
-	return nil
+	return http.ListenAndServe(s.config.Port, s.router)
 }
 
-// ConfigureLogger
 func (s *APIServer) configureLogger() error {
 	level, err := logrus.ParseLevel(s.config.LogLevel)
 	if err != nil {
@@ -36,4 +51,26 @@ func (s *APIServer) configureLogger() error {
 	s.logger.SetLevel(level)
 
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+}
+
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+
+	return nil
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello")
+		s.logger.Info("saying hello")
+	}
 }
--- internal/app/apiserver/config.go
+++ internal/app/apiserver/config.go
@@ -1,9 +1,12 @@
 package apiserver
 
+import "github.com/Rbd3178/filmDatabase/internal/app/store"
+
 // Config
 type Config struct {
-	Port     string `json:"port"`
-	LogLevel string `json:"log_level"`
+	Port     string `toml:"port"`
+	LogLevel string `toml:"log_level"`
+	Store *store.Config
 }
 
 // NewConfig
@@ -11,5 +14,6 @@ func NewConfig() *Config {
 	return &Config{
 		Port:     "8080",
 		LogLevel: "debug",
+		Store: store.NewConfig(),
 	}
 }
