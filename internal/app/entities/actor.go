798bbeba192c0402e69238e0d79dfbf490b0369e
Ilya V <ilya.vorontsov.2004@gmail.com>
2024-03-18 01:53:30 +0300

    format

--- internal/app/entities/actor.go
+++ internal/app/entities/actor.go
@@ -2,18 +2,18 @@ package entities
 
 // ActorWithFilm
 type ActorWithFilm struct {
-	ID int `db:"id"`
-	Name string `db:"name"`
-	Gender string `db:"gender"`
-	BirthDate string `db:"birth_date"`
-	FilmId *int `db:"film_id"`
-	Title *string `db:"title"`
+	ID        int     `db:"id"`
+	Name      string  `db:"name"`
+	Gender    string  `db:"gender"`
+	BirthDate string  `db:"birth_date"`
+	FilmId    *int    `db:"film_id"`
+	Title     *string `db:"title"`
 }
 
 // Actor
 type Actor struct {
-	ID int `db:"id"`
-	Name string `db:"name"`
-	Gender string `db:"gender"`
+	ID        int    `db:"id"`
+	Name      string `db:"name"`
+	Gender    string `db:"gender"`
 	BirthDate string `db:"birth_date"`
 }
